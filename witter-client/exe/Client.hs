2b1b5bb30229fd016e246cbf69a183e3a6d789d4
Michael Gale <m.gale@warwick.ac.uk>
2019-03-12 16:26:37 +0000

    Reduce code to starter code

--- witter-client/exe/Client.hs
+++ witter-client/exe/Client.hs
@@ -16,32 +16,6 @@ import Witter.API
 
 --------------------------------------------------------------------------------
     
--- | `witterEnv` @manager@ is a `ClientEnv` value for the Witter REST API,
--- configured for @localhost:8081@ where @manager@ is used for connections.
-witterEnv :: Manager -> ClientEnv 
-witterEnv manager = 
-    ClientEnv manager (BaseUrl Http "localhost" 8081 "") Nothing
-    
--- | `runWitterClient` @action@ runs @action@ against the Witter API using a 
--- new `Manager`.
-runWitterClient :: ClientM a -> IO (Either ServantError a)
-runWitterClient action = do 
-    manager <- newManager defaultManagerSettings 
-    runClientM action (witterEnv manager)
-    
---------------------------------------------------------------------------------
-    
-getWeets :<|> 
-    getWeet :<|>
-    getWeetsBefore :<|>
-    getWeetsOn :<|>
-    getUser :<|>
-    getUserWeets :<|>
-    getFollowers :<|> 
-    getFollowing :<|>
-    getUsers :<|> 
-    getUsersBefore :<|>
-    postWeet = client witterApi
 
     
 --------------------------------------------------------------------------------
